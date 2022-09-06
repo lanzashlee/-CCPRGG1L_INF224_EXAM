e12d5cf22e5b69186015eb2bf80484dea8bc5af1
Lanz Ashlee Ricamara <111546362+lanzashlee@users.noreply.github.com>
2022-09-06 09:32:24 +0800

    Update Student.java

--- exam_1/Student.java
+++ exam_1/Student.java
@@ -10,11 +10,7 @@ public class Person {
 
     // Student Method
     public void Lanz(){
-        System.out.println(" My Surname is " + surname);
-        System.out.println(" My first name is " + firstName);
-        System.out.println(" My middle minitial is " + middleinitial);
         System.out.println(" My ST number is " + studentNumber);
-        System.out.println(" My birthday is " + dateOfBirth);
         System.out.println(" My email address is " + studentEmailAddress);
         System.out.println(" I am awesome " + iAmAwesome);
     }
