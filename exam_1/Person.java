e6f253487b97774a3a80d0e3d5eab00b8c7de041
Lanz Ashlee Ricamara <111546362+lanzashlee@users.noreply.github.com>
2022-09-06 09:26:00 +0800

    Create Person.java

---
+++ exam_1/Person.java
@@ -0,0 +1,21 @@
+public class Person {
+    //student properties
+    String surname;
+    String firstName;
+    char middleinitial;
+    int studentNumber;
+    String dateOfBirth;
+    String studentEmailAddress;
+    boolean iAmAwesome;
+
+    // Student Method
+    public void Lanz(){
+        System.out.println(" My Surname is " + surname);
+        System.out.println(" My first name is " + firstName);
+        System.out.println(" My middle minitial is " + middleinitial);
+        System.out.println(" My ST number is " + studentNumber);
+        System.out.println(" My birthday is " + dateOfBirth);
+        System.out.println(" My email address is " + studentEmailAddress);
+        System.out.println(" I am awesome " + iAmAwesome);
+    }
+}
