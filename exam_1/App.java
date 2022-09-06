9789550190230c5dffcd48a45b06ad4203e8e0f9
Lanz Ashlee Ricamara <111546362+lanzashlee@users.noreply.github.com>
2022-09-06 09:25:11 +0800

    Create App.java

---
+++ exam_1/App.java
@@ -0,0 +1,16 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+         //Person Class
+         Person Lanz = new Person();
+
+         Lanz.surname = "Ricamara";
+         Lanz.firstName = "Lanz Ashlee";
+         Lanz.middleinitial = 'D';
+         Lanz.dateOfBirth = "05/17/2004";
+         Lanz.studentNumber = 2022104474;
+         Lanz.studentEmailAddress = "ricamarald@students.national-u.edu.ph";
+         Lanz.iAmAwesome = true;
+         Lanz.Lanz();
+       
+    }
+}
